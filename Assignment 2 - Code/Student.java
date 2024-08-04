cc4d096579d55871e6e94108e6dc4d2b715d57ce
My Name <dahal.barsha07@gmail.com>
2024-08-04 22:02:35 +1000

    Student Class Initialized and Necessary Library Imported
    Student Class Initialized and Necessary Library Imported

---
+++ Assignment 2 - Code/Student.java
@@ -0,0 +1,21 @@
+
+/**
+ * The java program reads data from file that contains student's name, 
+ * ID, marks for three assignments, system can calculate total marks 
+ * for each student, filter students based on total marks and sort 
+ * them to find top and bottom performers.
+ * Student class represents student with detail and their marks.
+ * Author: Barsha Dahal
+ * Date: 4th August, 2024
+ */
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Student
+{
+   
+}
